5097eaacefc79e029996e44ed7dd471e33fec199
SaFed23 <04.safed.04@gmail.com>
2019-11-26 22:20:06 +0300

    fix bug in 3/3

--- 3/3.py
+++ 3/3.py
@@ -1,10 +1,10 @@
 import random
 
 
-k = int(input("Введите наименьшее число: "))
-n = int(input("Введите наибольшее число: "))
+k = int(input("Введите наименьший индекс: "))
+n = int(input("Введите наибольший индекс: "))
 arr = [random.randint(0, 100) for i in range(int(input("Введите количество элементов: ")))]
 print(arr)
-for num in arr:
-    if k <= num <= n:
-        print(num)
+for i in range(len(arr)):
+    if k <= i <= n:
+        print(arr[i])
